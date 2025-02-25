01c12601807825fd39539711527ce3c29d71e57c
Talha7138 <ismetsert@outlook.com>
2025-02-25 13:11:11 +0300

    25.02.2025-Projeye Devam

--- RealEstate_2_API/Repositories/ProductRepositories/ProductRepo.cs
+++ RealEstate_2_API/Repositories/ProductRepositories/ProductRepo.cs
@@ -16,7 +16,7 @@ namespace RealEstate_2_API.Repositories.ProductRepositories
        
         public async Task<List<ResultProductDto>> GetProductsAsync()
         {
-            string query = "select * from Product";
+            string query = "select * from ProductTwo";
             using (var context = _context.CreateConnection()) 
             {
                 var values = await context.QueryAsync<ResultProductDto>(query);
@@ -28,7 +28,7 @@ namespace RealEstate_2_API.Repositories.ProductRepositories
 
         public async Task<List<ResultProductWithCategoryDto>> GetAllProductWithAsync()
         {
-            string query = "select ProductID, ProductTitle, ProductPrice, ProductCity, ProductDistrict, CategoryName from Product inner join Category on Product.ProductCategory = Category.CategoryID";
+            string query = "select ProductID, ProductTitle, ProductPrice, ProductImage, ProductAddress, ProductCity, ProductDistrict, CategoryName from ProductTwo inner join Category on ProductTwo.ProductCategory = Category.CategoryID";
             using (var connection = _context.CreateConnection())
             {
                 var values = await connection.QueryAsync<ResultProductWithCategoryDto>(query);
